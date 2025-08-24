f3a805abf984cca0818ce8378b9bb22ecea07bd4
cague <cague@192.168.1.36>
2025-08-23 20:00:21 -0500

    carga inicial

---
+++ src/tareaparcial/TareaParcial.java
@@ -0,0 +1,33 @@
+
+package tareaparcial;
+//Paquete de datos e/s datos por consola
+
+import java.util.Scanner;
+public class TareaParcial {
+    //Metodo principal
+    public static void main(String args []) {
+        // Declaracion de variables
+        
+    String empleado;
+    double ht, th, sb, d, sn;
+    //creando el objeto lectura
+    Scanner lectura= new Scanner(System.in);
+    //Entrada de datos
+    System.out.print("Nombre de empleado: ");
+    empleado=lectura.next();
+    System.out.print("Horas trabajadas: ");
+    ht = lectura.nextDouble();
+    System.out.print("Tarifa por hora: ");
+    th=lectura.nextDouble();
+    //Proceso de datos
+    sb=ht*th;
+    d=sb*0.13;
+    sn=sb-d;
+    //Salida de datos
+    System.out.println("Sueldo bruto: "+sb);
+    System.out.println("Descuento: "+d);
+    System.out.println("Sueldo Neto: "+sn);
+    
+    }//Fin del método
+    
+}//Fin del programa
